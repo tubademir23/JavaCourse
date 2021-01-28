9a71c1ce262a1709eab31583ac94c3ca4545ab9f
tubademir23 <tubademir23@gmail.com>
2021-01-28 14:39:09 +0300

    define bean under Class config

--- springDemoIoCAnnotation/src/com/springdemo/IoCConfig.java
+++ springDemoIoCAnnotation/src/com/springdemo/IoCConfig.java
@@ -1,5 +1,6 @@
 package com.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,5 +8,8 @@ import org.springframework.context.annotation.Configuration;
 //<context:component-scan base-package="com.springdemo"></context:component-scan>
 @ComponentScan("com.springdemo")
 public class IoCConfig {
-
+	@Bean
+	public ICustomerDal database() {
+		return new  MySqlCustomerDal();
+	}
 }
