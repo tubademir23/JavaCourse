444ca75646ecb1e7adf770d8d77601c08df0f203
tubademir23 <tubademir23@gmail.com>
2021-01-28 16:30:56 +0300

    update sample

--- hibernateDemo/src/com/hibernateDemo/Main.java
+++ hibernateDemo/src/com/hibernateDemo/Main.java
@@ -16,15 +16,23 @@ public class Main {
 		try {
 			session.beginTransaction();
 			//HQL-> hibernate query language
-			/*List<String> cities=session.createQuery("select c.countryCode from City c where c.name like 'A%' group by c.countryCode").getResultList();
+			/*Querying
+			 * 
+			 * List<String> cities=session.createQuery("select c.countryCode from City c where c.name like 'A%' group by c.countryCode").getResultList();
 			
 			for(String city:cities) {
 				System.out.println(city);
 			}*/
+			/*
+			 * inserting
 			City city=new City(4080,"test city","TUR", "Test city",5000000);
 			session.save(city);
-			session.getTransaction().commit();
 			System.out.println("city added");
+			*/
+			City city=session.get(City.class, 4080);
+			city.setCountryCode("SUR");
+			session.getTransaction().commit();
+			System.out.println("city updated "+ city.getCountryCode());
 		}finally {
 			factory.close();
 		}
