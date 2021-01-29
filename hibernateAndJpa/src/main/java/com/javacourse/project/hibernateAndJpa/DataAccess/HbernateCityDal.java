c8f99807a1c21ce8bf8ef4891ca6462ab65472bf
tubademir23 <tubademir23@gmail.com>
2021-01-29 13:39:53 +0300

    JPA and AOP Implementation

---
+++ hibernateAndJpa/src/main/java/com/javacourse/project/hibernateAndJpa/DataAccess/HbernateCityDal.java
@@ -0,0 +1,49 @@
+package com.javacourse.project.hibernateAndJpa.DataAccess;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.Session;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.javacourse.project.hibernateAndJpa.Entities.City;
+
+public class HbernateCityDal implements ICityDal{
+
+	private EntityManager entityManager;
+	
+	@Autowired
+	public HbernateCityDal(EntityManager entityManager) {
+		
+		this.entityManager = entityManager;
+	}
+	//AOP: Aspect Orianted Programming-code build olunca arkasına transaction koyuyor.
+	@Override
+	@Transactional
+	public List<City> GetAll() {
+		Session session=entityManager.unwrap(Session.class);
+		List<City> cities=session.createQuery("from city", City.class).getResultList();
+		return cities;
+	}
+
+	@Override
+	public void add(City city) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(City city) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(City city) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ hibernateAndJpa/src/main/java/com/javacourse/project/hibernateAndJpa/DataAccess/ICityDal.java
@@ -0,0 +1,12 @@
+package com.javacourse.project.hibernateAndJpa.DataAccess;
+
+import java.util.List;
+
+import com.javacourse.project.hibernateAndJpa.Entities.*;
+public interface ICityDal {
+
+	List<City> GetAll();
+	void add(City city);
+	void update(City city);
+	void delete(City city);
+}
