9fbf8730d3266e39dd3dfec3c24b36d6b59d53bf
tubademir23 <tubademir23@gmail.com>
2021-01-29 10:45:18 +0300

    maven sample

---
+++ intro/src/test/java/com/abc/intro/AppTest.java
@@ -0,0 +1,25 @@
+package com.abc.intro;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+	public AppTest(String testName) {
+		super(testName);
+	}
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
